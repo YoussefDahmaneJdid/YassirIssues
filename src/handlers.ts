24bf8fd7acc0f582f3eba4e8b53a5314924b5347
Youssef-Dahmane Jdid <youssef@shopsandco.com>
2022-02-15 18:40:07 +0100

    issue fixed2

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,5 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { PokemonWithStats } from "models/PokemonWithStats";
+const fs = require('fs')
+const requests = require("request");
+
 
 export async function getPokemonByName(request: FastifyRequest, reply: FastifyReply) {
   var name: string = request.params['name']
@@ -8,51 +11,68 @@ export async function getPokemonByName(request: FastifyRequest, reply: FastifyRe
 
   var urlApiPokeman = `https://pokeapi.co/api/v2/pokemon/`;
 
-  var params = {}
-
-  name == null
+ /* name == null
       ? name.trim() != ''
       ? (params["name"] = name, urlApiPokeman = urlApiPokeman + '/', urlApiPokeman = urlApiPokeman + name)
       : (urlApiPokeman = urlApiPokeman + '"?offset=20"', urlApiPokeman = urlApiPokeman + "&limit=20")
-      : (urlApiPokeman = urlApiPokeman + '"?offset=20"', urlApiPokeman = urlApiPokeman + "&limit=20")
+      : (urlApiPokeman = urlApiPokeman + '"?offset=20"', urlApiPokeman = urlApiPokeman + "&limit=20")*/
 
+
+  urlApiPokeman = urlApiPokeman+name+'/?offset=20&limit=20';
   const http = require('http');
   const keepAliveAgent = new http.Agent({ keepAlive: true });
 
   let response: any = ""
+  console.log(urlApiPokeman);
+   
 
-  http.request({ ...reply.headers, ...({ hostname: urlApiPokeman, port: 80, }) }, (result) => { response = result })
+  requests.get(urlApiPokeman, (error, response, body) => {
+         let json = JSON.parse(body);
+         computeResponse(json, reply);
+         reply.send(json)
+        
+});
+
+   
+     
 
   if (response == null) {
     reply.code(404)
   }
+  
+ 
 
-  computeResponse(response, reply)
+  
 
-  reply.send(response)
-
-  return reply
+  return reply;
 }
 
 export const computeResponse = async (response: unknown | any, reply: FastifyReply) => {
   const resp = response as any
+  let listUrl=[];
+   resp.types.map(type => type.type).map(type => { 
+   listUrl.push(type.url);
+    return type.url;
+  }) || [];
 
-  let types = resp.types.map(type => type.type).map(type => { return type.url }).reduce((types, typeUrl) => types.push(typeUrl));
+ 
 
   let pokemonTypes = []
 
-  types.forEach(element => {
-    const http = require('http');
-    const keepAliveAgent = new http.Agent({ keepAlive: true });
+  listUrl.forEach(element => {
+    requests.get(element, (error, response, body) => {
+      let json = JSON.parse(body);
+      pokemonTypes.push(json);
+     });
+     });
 
-    http.request({ hostname: element }, (response) => pokemonTypes.push(response))
-
-  });
+   
+ 
 
   if (pokemonTypes == undefined)
     throw pokemonTypes
-   
-  response.stats.forEach(element => {
+
+ /* response.stats.forEach(element => {
     var stats = []
 
     pokemonTypes.map(pok =>
@@ -69,6 +89,6 @@ export const computeResponse = async (response: unknown | any, reply: FastifyRep
     } else {
       element.averageStat = 0
     }
-  });  
+  });  */
 
 }
